0c7ded3b731b24a2f1daa134fdc368cef30dcf27
rupen11 <jarsaniyarupen7984@gmail.com>
2022-07-15 14:57:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
 function App() {
+
+  const [info, setInfo] = useState([]);
+  const [data, setData] = useState([]);
+  const [tempp, setTempp] = useState("fah");
+  const [page, setPage] = useState(3);
+
+  const handleTemp = e => setTempp(e.target.value);
+
+  const pagination = () => {
+
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40", {
+        method: "GET",
+        headers: {
+          "content-type": "application/json"
+        }
+      });
+
+      if (res.status === 200) {
+        const json = await res.json();
+        setInfo(json.list);
+      }
+    }
+    try {
+      // setTimeout(() => {
+      fetchData();
+      // }, 2000); //set time for see the loading (just for)
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }, [])
+
+  // c = (f - 32) * (5/9)
+  // f = (c * 1.8) + 32
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {
+        info.length !== 0 ?
+          <div className="flex">
+            <h1>Good Weather</h1>
+
+            <div className="flex-switch">
+              <span>
+                <input type="radio" name="temp" value="cel" onChange={handleTemp} /> Cel
+              </span>
+              <span>
+                <input type="radio" name="temp" value="fah" onChange={handleTemp} defaultChecked /> Fah
+              </span>
+            </div>
+
+            <div className="arrwos">
+              <div className="arrow-left">
+                <i className="fa fa-arrow-left" onClick={pagination}></i>
+              </div>
+              <div className="arrow-right">
+                <i className="fa fa-arrow-right" onClick={pagination}></i>
+
+              </div>
+            </div>
+
+            <div className="weather-box">
+              {
+                console.log(data)
+                // data?.map((data, index) => {
+                //   if (index + 1 <= page) {
+                //     return <div className="innerbox" key={index + 1} >
+                //       <div className="temp">
+                //         Temp: {tempp === "cel" ? parseInt((data.main.temp - 32) * (5 / 9)) + "°C" : data.main.temp + "°F"}
+                //       </div>
+                //       <div className="date">
+                //         Date: {new Date(data.dt_txt).toLocaleDateString()}
+                //       </div>
+                //       <div className="weather">
+                //         Weather: {data.weather[0].main}
+                //       </div>
+                //     </div>
+                //   }
+                // })
+
+              }
+            </div>
+          </div>
+          : <h1> Please wait, while we are fetching data for you.</h1>
+      }
+
+    </>
   );
 }
 
